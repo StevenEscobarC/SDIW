fdcea52deea57ed0ac88abaa65cb60f9ec0761bc
SAGS11 <sergiogomezsilva1011@gmail.com>
2020-01-16 20:16:44 -0500

    Validación y Login

--- client/src/app/modules/security/login/login.component.ts
+++ client/src/app/modules/security/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  fgValidation: FormGroup;
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  fgValidationBuilder() { 
+    this.fgValidation = this.fb.group({
+      username:['admin',[Validators.minLength(5),Validators.maxLength(40), Validators.email], ],
+      password: ['',[Validators.required,Validators.minLength(10),Validators.maxLength(15),Validators.pattern("")]],
+    });
+  }
   ngOnInit() {
   }
 
